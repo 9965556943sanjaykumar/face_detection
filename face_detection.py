ae22b70ffd8fb9e55fcfb348e019459c09adea39
9965556943sanjaykumar <94106618+9965556943sanjaykumar@users.noreply.github.com>
2024-08-13 19:50:26 +0530

    Add files via upload

---
+++ face_detection.py
@@ -0,0 +1,15 @@
+import cv2 #importing cv2 module
+a=cv2.CascadeClassifier("haarcascade_frontalface_default.xml")  #extracting cascade source file
+b=cv2.VideoCapture(0)  #capyure the video
+while True:
+    c_rec,d_image=b.read()
+    e=cv2.cvtColor(d_image,cv2.COLOR_BGR2GRAY)
+    f=a.detectMultiScale(e,1.3,6)
+    for (x1,y1,w1,h1) in f:
+         cv2.rectangle(d_image,(x1,y1),(x1+w1,y1+h1),(255,0,0),5)   #5 is weight of rec
+    cv2.imshow('img',d_image)
+    h=cv2.waitKey(40) & 0xff
+    if h==40:
+        break
+b.release()    #release the camera
+cv2.destroyAllWindows()  #closing the module
